212a18f598dc5fd292c86cafc2bccbdb1c40ed86
Patryk <patryk.rudownik@gmail.com>
2023-07-04 01:21:39 +0200

    Entities Core -> Sensor and Reading

---
+++ Domain/Entities/Sensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Sensor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string IpAddress { get; set; }
+        public bool IsEnabled { get; set; }
+        
+    }
+}
