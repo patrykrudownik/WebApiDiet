23805a8566a741cd757db6368c62ed72fdd28d33
Patryk <patryk.rudownik@gmail.com>
2023-10-27 01:10:56 +0200

    Entities + Interfaces

--- Domain/Entities/Ingredient.cs
+++ Domain/Entities/Ingredient.cs
@@ -6,9 +6,16 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class Ingredient
+    public class Ingredient
     {
         public int Id { get; set; }
-        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Kcal {  get; set; }
+        public double Protein { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Fat { get; set; }
+
+        Ingredient() { }
     }
 }
